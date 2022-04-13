47bb4ead6df34e00474b85090314f4bf3a2ebd92
Jefferson <wakesseh@gmail.com>
2022-04-13 16:21:00 -0400

    frontend starterpack done

--- README.md
+++ README.md
@@ -5,8 +5,10 @@
 [deployed url](http://url-if-deployed-here)
 
 ## What Worked Well
+The instructions were straightforward to follow.
 
 ## What Didn't
+I didn't encounter any significant challenges.
 
 ## Extra Credit
 
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let seconds = 0;
+
+setInterval(() => {
+  seconds += 1;
+  $('#main').html(`You've been on this page for ${seconds} seconds.`);
+}, 1000);
