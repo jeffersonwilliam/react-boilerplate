f3da69985bed88be008e3fd9647fc7daa1cc5c91
Jefferson <wakesseh@gmail.com>
2022-04-24 09:50:49 -0400

    added routing elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import './style.scss';
 
-let seconds = 0;
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
 
-setInterval(() => {
-  seconds += 1;
-  $('#main').html(`You've been on this page for ${seconds} seconds.`);
-}, 1000);
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/ exact">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
