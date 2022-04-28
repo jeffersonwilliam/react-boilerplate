7032e209b73b1466ba80ddb3399823eb13d2b225
Jefferson <wakesseh@gmail.com>
2022-04-27 23:08:56 -0400

    added redux

---
+++ src/components/app.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
+import Counter from './counter';
+import Controls from './controls';
+import '../style.scss';
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return (
+    <div>Welcome
+      <Counter />
+      <Controls />
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/ exact">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
